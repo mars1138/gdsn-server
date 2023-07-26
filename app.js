5d09848962589a254e550bec2503d896164bf7fc
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-07-26 16:54:52 -0700

    setup app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const productRoutes = require('./routes/products-routes');
-const userRoutes = require('./routes/users-routes');
+const productsRoutes = require('./routes/products-routes');
+const usersRoutes = require('./routes/users-routes');
 const contactRoutes = require('./routes/contact-routes');
 
 const app = express();
@@ -14,3 +14,32 @@ const cors = require('cors');
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_URL}`,
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  })
+);
+
+app.use('/api/products', productsRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/contact', contactRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ykppkft.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => app.listen(process.env.PORT || 5000))
+  .catch((err) => console.log(err));
